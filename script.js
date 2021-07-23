390a4259978a57b3dd7ec3621af76b2924c1ae26
Ilya Kudrytski <ilya.kudrytski@gmail.com>
2021-07-23 21:42:24 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+const addBtn = document.querySelector('#addBtn');
+
+
+addBtn.addEventListener('click', generateP);
+
+async function serverQuery(url) {
+    const respond = await fetch(url);
+    return respond.text();
+}
+let counter = 1;
+async function generateP() {
+
+
+    const data = await serverQuery(`service${counter}.html`);
+
+    const pEl = document.querySelector('#result');
+    pEl.innerHTML = data;
+    counter++;
+
+}
+
+// let result = document.getElementById('result');
+// let button = document.getElementById('addBtn');
+
+// button.addEventListener('click', function() {
+//     let promise = fetch('service.html');
+
+//     promise.then(
+//         response => {
+//             return response.text();
+//         }
+//     ).then(
+//         text => {
+//             result.innerHTML = text;
+//         }
+//     );
+// });
